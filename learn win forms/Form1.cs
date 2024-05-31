26bb0d7b2a80bcf60ef931d99ae01ef9bf7daff0
whoami <az.khabibullin2018@gmail.com>
2024-05-31 20:17:58 +0500

    v1

--- learn win forms/Form1.cs
+++ learn win forms/Form1.cs
@@ -12,9 +12,56 @@ namespace learn_win_forms
 {
     public partial class Form1 : Form
     {
+        class Person
+        {
+            public string FirstName { get; set; }
+            public string MiddleName { get; set; }
+            public string SecondName { get; set; }
+
+            public string GetFullName()
+            {
+                return $"Фамилия: {SecondName} | Имя: {FirstName} | Отчество: {MiddleName}";
+            }
+        }
+
+        private List<Person> GetPersons()
+        {
+            return new List<Person>()
+            {
+                new Person()
+                {
+                    FirstName = "Джеймс",
+                    MiddleName = "Эдвард",
+                    SecondName = "Вудс"
+                },
+                new Person()
+                {
+                    FirstName = "Кэтрин",
+                    MiddleName = "Элизабет",
+                    SecondName = "Кэмпбелл"
+                },
+                new Person()
+                {
+                    FirstName = "Кристофер",
+                    MiddleName = "Джеймс",
+                    SecondName = "Роу"
+                }
+            };
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            var persons = GetPersons();
+            foreach (var person in persons)
+            {
+                listBox1.Items.Add(person.GetFullName());
+            }
         }
     }
 }
